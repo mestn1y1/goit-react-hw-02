ddcd383a900e549a350b2fc787fcfe02b5a8ac2f
Serhii Tyron <mestn1y@hotmail.com>
2024-08-15 01:07:09 +0300

    hw2-done

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,10 +1,12 @@
+import css from "./Feedback.module.css";
+
 export default function Feedback({
   feedback,
   totalFeedback,
   positiveFeedback,
 }) {
   return (
-    <ul>
+    <ul className={css.container}>
       <li>Good: {feedback.good}</li>
       <li>Bad: {feedback.bad}</li>
       <li>Neutral: {feedback.neutral}</li>
